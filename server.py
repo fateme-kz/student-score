ff30ca3d2aa2161d085a3048261d9aed1a8e8f37
fateme-kz <fatemekhazaei17@gmail.com>
2024-08-10 17:20:47 +0330

    changes added

--- server.py
+++ server.py
@@ -1,20 +1,28 @@
-from flask import Flask ,request ,render_template
-from controler import stu_num
+from flask import Flask ,request ,render_template , redirect, url_for
 
 app = Flask(__name__, template_folder='template')
 
 
 @app.route('/')
 def input_number():
+    if request.method == 'POST':
+        score = request.form.get('score')
+        return redirect(url_for('call_function', count=score))
     return render_template('input.html')
 
 
-@app.route('/call_function', methods=['POST' ])
+@app.route('/call_function', methods=['POST', 'GET'])
 def call_function():
-    number = request.form.get('input_number', type=int)
-    result = stu_num(number)
-    return render_template('score.html')
+    count = request.form.get('input_number', type=int)
+    return render_template('score.html', count=count)
 
+@app.route('/call_average', methods=['POST'])
+def call_average():
+    names = request.form.getlist('name')
+    scores = request.form.getlist('score')
+    scores_float = list(map(float, scores))
+    average_scores = sum(scores_float) / len(scores_float) if scores_float else 0
+    return render_template('average.html', names=names, scores=scores, average=average_scores, zip=zip)
 
 if __name__ == '__main__':
     app.run(debug=True)
